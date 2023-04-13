9fb2af0f89db527446ce9219bd8eee630d694778
micah1crandell <micah1crandell@gmail.com>
2023-04-12 18:53:40 -0700

    starting over

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ container.appendChild(renderer.domElement);
 
 // Load the panoramic image and create a texture
 const loader = new THREE.TextureLoader();
-const texture = loader.load('modcomp.jpeg');
+const texture = loader.load('cozystudio.jpeg');
 
 // Create a spherical geometry and map the texture to it
 const geometry = new THREE.SphereGeometry(500, 60, 40);
